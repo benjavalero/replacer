5066aeb176098b1ef39955f75f4384212c7c307c
Benjamín Valero <benjavalero@gmail.com>
2016-09-30 00:23:25 +0200

    Initial refactoring of JS code in modules

---
+++ js/regex.js
@@ -0,0 +1,58 @@
+var RegEx = {
+
+	// JavaScript doesn't support regex lookbehind
+	// JavaScript doesn't support dotall flag. Workaround: . => [\\S\\s]
+
+	reCommentEncoded : new RegExp('(&lt;!--.+?--&gt;)', 'g'),
+
+	reHeader : new RegExp('(\\={2,}.+?\\={2,})', 'g'),
+
+	reHyperlink : new RegExp('(https?://[\\w\\./\\-\\+\\?&%=:#;]+)', 'g'),
+
+	reLink : new RegExp('(\\[\\[(.+?)(\\|.+)?\\]\\])', 'g'),
+
+	// \w doesn't include accentuated characters
+	wordCharacterClass : '\\wÁáÉéÍíÓóÚúÜüÑñ',
+
+	reTemplateParam : new RegExp('\\|[' + this.wordCharacterClass
+			+ '\\s]+(?=\\=)', 'g'),
+
+	reIndexValue : new RegExp('\\|\\s*índice\\s*=[\\S\\s]*?[\\}\\|]', 'g'),
+
+	reUnreplaceableTemplate : new RegExp(
+			'\\{\\{(?:ORDENAR:|DEFAULTSORT:|NF\\|)[^\\}]*', 'g'),
+
+	reTemplateName : new RegExp('\\{\\{[^\\|\\}]+', 'g'),
+
+	reQuote : new RegExp('\\{\\{(?:[Cc]ita|c?Quote)\\|[^\\}]*', 'g'),
+
+	reQuotes : new RegExp("'{2,5}.+?'{2,5}", 'g'),
+
+	reAngularQuotes : new RegExp('«[^»]+»', 'g'),
+
+	reTypographicQuotes : new RegExp('“[^”]+”', 'g'),
+
+	reDoubleQuotes : new RegExp('"[^"]+"', 'g'),
+
+	reFileName : new RegExp(
+			'[\\=\\|:][^\\=\\|:]+\\.(?:svg|jpe?g|JPG|png|PNG|gif|ogg|pdf)', 'g'),
+
+	reRefName : new RegExp('<ref[^>]*>', 'g'),
+
+	reCategory : new RegExp('\\[\\[Categoría:.*?\\]\\]', 'g'),
+
+	reComment : new RegExp('<!--[\\S\\s]*?-->', 'g'),
+
+	reFalsePositives : new RegExp(
+			'[Ss]ólo|[Ii]ndex|[Ll]ink|[Oo]nline|[Rr]eferences?|Jean|[Aa]un así',
+			'g'),
+
+	/* Exceptions where misspellings will be ignored */
+	exceptionRegExps : [ this.reTemplateParam, this.reIndexValue,
+			this.reUnreplaceableTemplate, this.reTemplateName, this.reQuote,
+			this.reQuotes, this.reAngularQuotes, this.reTypographicQuotes,
+			this.reDoubleQuotes, this.reFileName, this.reRefName,
+			this.reCategory, this.reComment, this.reHyperlink,
+			this.reFalsePositives ]
+
+};
