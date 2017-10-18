fe7205dd0cfe15193703f352fde60abe5d2cbc16
Benjamín Valero <benjavalero@gmail.com>
2017-10-19 00:20:20 +0200

    New exception: template parameters.

---
+++ src/test/java/es/bvalero/replacer/article/exception/TemplateParamFinderTest.java
@@ -0,0 +1,25 @@
+package es.bvalero.replacer.article.exception;
+
+import es.bvalero.replacer.utils.RegexMatch;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class TemplateParamFinderTest {
+
+    @Test
+    public void testRegexTemplateParam() {
+        String text = "xxx {{Template| param1 = value1 | parám_ 2 = value2|param-3=|param4 }} {{Cita|Alea iacta est}} jajaja =";
+
+        TemplateParamFinder templateParamFinder = new TemplateParamFinder();
+        List<RegexMatch> matches = templateParamFinder.findErrorExceptions(text);
+
+        Assert.assertFalse(matches.isEmpty());
+        Assert.assertEquals(3, matches.size());
+        Assert.assertTrue(matches.contains(new RegexMatch(14, "| param1 =")));
+        Assert.assertTrue(matches.contains(new RegexMatch(32, "| parám_ 2 =")));
+        Assert.assertTrue(matches.contains(new RegexMatch(51, "|param-3=")));
+    }
+
+}
