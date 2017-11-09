77b77b06b7bd84502071839301643db586e1424d
Benjamín Valero <benjavalero@gmail.com>
2017-11-09 20:08:48 +0100

    Fix exception for comments with tags inside

---
+++ src/test/java/es/bvalero/replacer/article/exception/CommentFinderTest.java
@@ -0,0 +1,48 @@
+package es.bvalero.replacer.article.exception;
+
+import es.bvalero.replacer.utils.RegexMatch;
+import es.bvalero.replacer.utils.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class CommentFinderTest {
+
+    @Test
+    public void testRegexComment() {
+        String comment = "<!-- Esto es un \n comentario -->";
+        String text = "xxx " + comment + " zzz";
+
+        CommentFinder commentFinder = new CommentFinder();
+        List<RegexMatch> matches = commentFinder.findErrorExceptions(text);
+
+        Assert.assertFalse(matches.isEmpty());
+        Assert.assertEquals(comment, matches.get(0).getOriginalText());
+    }
+
+    @Test
+    public void testRegexCommentWithTagsInside() {
+        String comment = "<!-- Esto <span>es</span> un \n comentario -->";
+        String text = "xxx " + comment + " zzz";
+
+        CommentFinder commentFinder = new CommentFinder();
+        List<RegexMatch> matches = commentFinder.findErrorExceptions(text);
+
+        Assert.assertFalse(matches.isEmpty());
+        Assert.assertEquals(comment, matches.get(0).getOriginalText());
+    }
+
+    @Test
+    public void testRegexCommentEscaped() {
+        String comment = "<!-- Esto es un \n comentario -->";
+        String text = "xxx " + comment + " zzz";
+
+        CommentFinder commentFinder = new CommentFinder();
+        List<RegexMatch> matches = commentFinder.findErrorExceptions(StringUtils.escapeText(text));
+
+        Assert.assertFalse(matches.isEmpty());
+        Assert.assertEquals(StringUtils.escapeText(comment), matches.get(0).getOriginalText());
+    }
+
+}
