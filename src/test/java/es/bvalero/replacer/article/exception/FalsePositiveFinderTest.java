eee767b5f8d377e164fcc914bad3c1a03f275ec3
Benjamín Valero <benjavalero@gmail.com>
2017-12-24 18:07:43 +0100

    More false positives

--- src/test/java/es/bvalero/replacer/article/exception/FalsePositiveFinderTest.java
+++ src/test/java/es/bvalero/replacer/article/exception/FalsePositiveFinderTest.java
@@ -19,13 +19,13 @@ public class FalsePositiveFinderTest {
 
     @Test
     public void testRegexFalsePositives() {
-        String text = "Un Link de Éstas en el Index Online de ésta Tropicos.org Aquél aquéllo.";
+        String text = "Un Link de Éstas en el Index Online de ésta Tropicos.org Aquél aquéllo Saint Martin.";
 
         FalsePositiveFinder falsePositiveFinder = new FalsePositiveFinder();
         List<RegexMatch> matches = falsePositiveFinder.findErrorExceptions(text);
 
         Assert.assertFalse(matches.isEmpty());
-        Assert.assertEquals(8, matches.size());
+        Assert.assertEquals(9, matches.size());
         Assert.assertTrue(matches.contains(new RegexMatch(3, "Link")));
         Assert.assertTrue(matches.contains(new RegexMatch(11, "Éstas")));
         Assert.assertTrue(matches.contains(new RegexMatch(23, "Index")));
@@ -34,6 +34,7 @@ public class FalsePositiveFinderTest {
         Assert.assertTrue(matches.contains(new RegexMatch(44, "Tropicos.org")));
         Assert.assertTrue(matches.contains(new RegexMatch(57, "Aquél")));
         Assert.assertTrue(matches.contains(new RegexMatch(63, "aquéllo")));
+        Assert.assertTrue(matches.contains(new RegexMatch(71, "Saint Martin")));
     }
 
 }
