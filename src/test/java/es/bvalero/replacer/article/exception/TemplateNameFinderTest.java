79951f2181b46453a7b948222100f692e6edd752
Benjamín Valero <benjavalero@gmail.com>
2018-08-19 09:56:32 +0200

    More regex improvements after experiments

--- src/test/java/es/bvalero/replacer/article/exception/TemplateNameFinderTest.java
+++ src/test/java/es/bvalero/replacer/article/exception/TemplateNameFinderTest.java
@@ -11,19 +11,25 @@ public class TemplateNameFinderTest {
 
     @Test
     public void testRegexTemplateParam() {
-        String text = "xxx {{Plantilla1| yyy }} zzz {{Plantilla2}}.";
+        String template1 = "{{Plantilla 1";
+        String template2 = "{{Plantilla\n 2";
+        String template3 = "{{Plantilla-3";
+
+        String text = "xxx " + template1 + "| yyy }} / " + template2 + "}} / " + template3 + ":zzz}}.";
 
         TemplateNameFinder templateNameFinder = new TemplateNameFinder();
 
         List<RegexMatch> matches = templateNameFinder.findExceptionMatches(text, false);
-        Assert.assertEquals(2, matches.size());
-        Assert.assertEquals("{{Plantilla1", matches.get(0).getOriginalText());
-        Assert.assertEquals("{{Plantilla2", matches.get(1).getOriginalText());
+        Assert.assertEquals(3, matches.size());
+        Assert.assertEquals(template1, matches.get(0).getOriginalText());
+        Assert.assertEquals(template2, matches.get(1).getOriginalText());
+        Assert.assertEquals(template3, matches.get(2).getOriginalText());
 
         matches = templateNameFinder.findExceptionMatches(StringUtils.escapeText(text), true);
-        Assert.assertEquals(2, matches.size());
-        Assert.assertEquals("{{Plantilla1", matches.get(0).getOriginalText());
-        Assert.assertEquals("{{Plantilla2", matches.get(1).getOriginalText());
+        Assert.assertEquals(3, matches.size());
+        Assert.assertEquals(StringUtils.escapeText(template1), matches.get(0).getOriginalText());
+        Assert.assertEquals(StringUtils.escapeText(template2), matches.get(1).getOriginalText());
+        Assert.assertEquals(StringUtils.escapeText(template3), matches.get(2).getOriginalText());
     }
 
 }
