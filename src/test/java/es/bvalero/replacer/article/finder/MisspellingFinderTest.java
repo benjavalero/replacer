3ed833139ae2c88ee3b0ac792e6462870d38f7ea
Benjamín Valero <benjavalero@gmail.com>
2018-08-31 15:40:00 +0200

    Improve tests

--- src/test/java/es/bvalero/replacer/article/finder/MisspellingFinderTest.java
+++ src/test/java/es/bvalero/replacer/article/finder/MisspellingFinderTest.java
@@ -32,7 +32,7 @@ public class MisspellingFinderTest {
 
     @Test
     public void testFindPotentialErrors() {
-        String articleContent = "UM vonito Exemplo";
+        String articleContent = "UM vonito Exemplo exemplo.";
 
         Misspelling misspelling1 = new Misspelling("um", false, "un");
         Mockito.when(misspellingManager.findMisspellingByWord("un")).thenReturn(misspelling1);
@@ -42,6 +42,7 @@ public class MisspellingFinderTest {
 
         Misspelling misspelling3 = new Misspelling("exemplo", false, "ejemplo");
         Mockito.when(misspellingManager.findMisspellingByWord("Exemplo")).thenReturn(misspelling3);
+        Mockito.when(misspellingManager.findMisspellingByWord("exemplo")).thenReturn(misspelling3);
 
         String misspellingRegex = "([Uu]n|[Vv]onito|[Ee]xemplo)";
         RunAutomaton misspellingAutomaton = new RunAutomaton(new RegExp(misspellingRegex).toAutomaton());
@@ -50,21 +51,31 @@ public class MisspellingFinderTest {
         List<ArticleReplacement> result = misspellingFinder.findPotentialErrors(articleContent);
 
         Assert.assertNotNull(result);
-        Assert.assertEquals(2, result.size());
+        Assert.assertEquals(3, result.size());
 
-        // "UN" will be ignored because it is all in uppercase and has a known misspelling
+        // "UN" will be ignored because it is all in uppercase and has not a known uppercase misspelling
         // Results are inverse order
 
         ArticleReplacement result1 = result.get(0);
         Assert.assertEquals("vonito", result1.getOriginalText());
         Assert.assertEquals(3, result1.getPosition());
         Assert.assertEquals(PotentialErrorType.MISSPELLING, result1.getType());
+        Assert.assertEquals("vonito", result1.getSubtype());
         Assert.assertEquals("bonito", result1.getProposedFixes().get(0));
 
         ArticleReplacement result2 = result.get(1);
         Assert.assertEquals("Exemplo", result2.getOriginalText());
         Assert.assertEquals(10, result2.getPosition());
+        Assert.assertEquals(PotentialErrorType.MISSPELLING, result2.getType());
+        Assert.assertEquals("exemplo", result2.getSubtype());
         Assert.assertEquals("Ejemplo", result2.getProposedFixes().get(0));
+
+        ArticleReplacement result3 = result.get(2);
+        Assert.assertEquals("exemplo", result3.getOriginalText());
+        Assert.assertEquals(18, result3.getPosition());
+        Assert.assertEquals(PotentialErrorType.MISSPELLING, result3.getType());
+        Assert.assertEquals("exemplo", result3.getSubtype());
+        Assert.assertEquals("ejemplo", result3.getProposedFixes().get(0));
     }
 
     @Test
