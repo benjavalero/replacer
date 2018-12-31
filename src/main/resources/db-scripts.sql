6777e235412d3c18a5243a1f839b8f50480b8926
Benjamín Valero <benjavalero@gmail.com>
2018-12-31 22:30:08 +0100

    Fix collation in database columns to distinguish diacritics

--- src/main/resources/db-scripts.sql
+++ src/main/resources/db-scripts.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS article;
 -- We cannot create an index or unique constraint for the title because the column is too long
 CREATE TABLE article (
     id INT NOT NULL,
-    title VARCHAR(255) NOT NULL,
+    title VARCHAR(255) COLLATE utf8mb4_bin NOT NULL,
     lastupdate DATE NOT NULL,
     PRIMARY KEY (id)
 );
@@ -13,7 +13,7 @@ CREATE TABLE replacement (
     id INT NOT NULL AUTO_INCREMENT,
     articleid INT NOT NULL,
     type VARCHAR(25) NOT NULL,
-    text VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+    text VARCHAR(30) COLLATE utf8mb4_bin NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (articleid, type, text),
     INDEX (type),
