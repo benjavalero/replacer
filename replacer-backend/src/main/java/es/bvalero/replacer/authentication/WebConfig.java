bdc8136f8c793feb053d9a9721ed2e802634209a
Benjamín Valero <benjavalero@gmail.com>
2019-03-22 00:28:15 +0100

    Disable CORS on development

---
+++ replacer-backend/src/main/java/es/bvalero/replacer/authentication/WebConfig.java
@@ -0,0 +1,23 @@
+package es.bvalero.replacer.authentication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Profile("offline")
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+
+}
