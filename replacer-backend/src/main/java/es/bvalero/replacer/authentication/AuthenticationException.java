78b91f7ef824d94cff14ddf73c84e24510518f2c
Benjamín Valero <benjavalero@gmail.com>
2019-05-07 13:57:31 +0200

    Remove unused method

--- replacer-backend/src/main/java/es/bvalero/replacer/authentication/AuthenticationException.java
+++ replacer-backend/src/main/java/es/bvalero/replacer/authentication/AuthenticationException.java
@@ -4,10 +4,6 @@ public class AuthenticationException extends Exception {
 
     private static final long serialVersionUID = -6299065674942122771L;
 
-    AuthenticationException(String message) {
-        super(message);
-    }
-
     AuthenticationException(Throwable throwable) {
         super(throwable);
     }
