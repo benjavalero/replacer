80de060f76d361e8cc4ac7ac82b56aa722a1a237
Benjamín Valero <benjavalero@gmail.com>
2019-06-06 16:18:35 +0200

    Fix scripts for offline use to new database model

--- replacer-backend/src/main/resources/data-h2.sql
+++ replacer-backend/src/main/resources/data-h2.sql
@@ -1,4 +1,4 @@
-INSERT INTO replacement2 (articleId, type, subtype) VALUES (1, 'MISSPELLING', 'aber');
-INSERT INTO replacement2 (articleId, type, subtype) VALUES (2, 'MISSPELLING', 'aber');
-INSERT INTO replacement2 (articleId, type, subtype) VALUES (2, 'MISSPELLING', 'madrid');
-INSERT INTO replacement2 (articleId, type, subtype) VALUES (2, 'MISSPELLING', 'paris');
+INSERT INTO replacement2 (article_id, type, subtype) VALUES (1, 'MISSPELLING', 'aber');
+INSERT INTO replacement2 (article_id, type, subtype) VALUES (2, 'MISSPELLING', 'aber');
+INSERT INTO replacement2 (article_id, type, subtype) VALUES (2, 'MISSPELLING', 'madrid');
+INSERT INTO replacement2 (article_id, type, subtype) VALUES (2, 'MISSPELLING', 'paris');
