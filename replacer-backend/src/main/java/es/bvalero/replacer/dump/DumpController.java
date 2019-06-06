6e1da47546db7a31c803f4f4d59d86978af1e844
Benjamín Valero <benjavalero@gmail.com>
2019-06-06 17:00:45 +0200

    Fix some Sonar issues in package Dump

--- replacer-backend/src/main/java/es/bvalero/replacer/dump/DumpController.java
+++ replacer-backend/src/main/java/es/bvalero/replacer/dump/DumpController.java
@@ -1,6 +1,7 @@
 package es.bvalero.replacer.dump;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * REST actions to start the dump processing or find the current status.
  */
 @RestController
+@RequestMapping("dump")
 public class DumpController {
 
     @Autowired
@@ -16,19 +18,19 @@ public class DumpController {
     @Autowired
     private DumpManager dumpManager;
 
-    @RequestMapping("/dump/status")
+    @GetMapping(value = "/status")
     public DumpProcessStatus getDumpStatus() {
         return dumpHandler.getProcessStatus();
     }
 
-    @RequestMapping("/dump/run")
+    @GetMapping(value = "/run")
     public boolean processLatestDumpFileManually() {
         // TODO : Estos métodos deberían devolver el true lo antes posible
         dumpManager.processLatestDumpFile(false);
         return true;
     }
 
-    @RequestMapping("/dump/run/force")
+    @GetMapping(value = "/run/force")
     public boolean processLatestDumpFileManuallyForced() {
         dumpManager.processLatestDumpFile(true);
         return true;
