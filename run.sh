bc90359cb9b8864feaf9b3c3e34176e15c2cb3e2
Benjamín Valero <benjavalero@gmail.com>
2019-07-07 17:59:20 +0200

    Add parameter to JVM to fix truncated indexation. Fixes #34.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $HOME
-exec java -XX:+HeapDumpOnOutOfMemoryError -jar $HOME/replacer/replacer-backend/target/replacer.jar
+exec java -Djdk.xml.totalEntitySizeLimit=0 -XX:+HeapDumpOnOutOfMemoryError -jar $HOME/replacer/replacer-backend/target/replacer.jar
