21d7e7fae1fd42e7c5a11d5f34d9cd80091ad236
Benjamín Valero <benjavalero@gmail.com>
2019-07-09 22:51:11 +0200

    Clear alerts on load custom replacements

--- replacer-frontend/src/app/article/find-custom.component.ts
+++ replacer-frontend/src/app/article/find-custom.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { AlertService } from '../alert/alert.service';
+
 @Component({
   selector: 'app-find-custom',
   templateUrl: './find-custom.component.html',
@@ -11,9 +13,10 @@ export class FindCustomComponent implements OnInit {
   replacement: string;
   suggestion: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private alertService: AlertService) { }
 
   ngOnInit() {
+    this.alertService.clearAlertMessages();
   }
 
   onSubmit() {
