b02bd04cb8aa8420028b321925dffcdd37f613b3
Benjamín Valero <benjavalero@gmail.com>
2019-09-25 11:25:25 +0200

    Set title on dashboard

--- replacer-frontend/src/app/dashboard/dashboard.component.ts
+++ replacer-frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 import { AuthenticationService } from '../authentication/authentication.service';
 
@@ -11,9 +12,11 @@ export class DashboardComponent implements OnInit {
 
   admin = false;
 
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(private authenticationService: AuthenticationService, private titleService: Title) { }
 
   ngOnInit() {
+    this.titleService.setTitle('Replacer - Reemplazador de la Wikipedia en español');
     this.admin = this.authenticationService.user.admin;
   }
+
 }
