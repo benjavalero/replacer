87788f49650c7cf32e2088ded9b7f48be73c3bf0
Benjamín Valero <benjavalero@gmail.com>
2019-12-04 11:04:13 +0100

    Update amount of pages in dump

--- replacer-frontend/src/app/app-const.ts
+++ replacer-frontend/src/app/app-const.ts
@@ -1,2 +1,2 @@
 export const PAGE_SIZE = 8; // Number of items per page in paginated tables
-export const NUM_ARTICLES = 3830338; // Rough amount of articles to be read
+export const NUM_ARTICLES = 3842425; // Rough amount of articles to be read
