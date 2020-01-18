bee47bbac246c1dafe84dc7d7ff4774c5aee6511
Benjamín Valero <benjavalero@gmail.com>
2020-01-18 22:58:01 +0100

    Add JavaDoc to domain objects

--- replacer-backend/src/main/java/es/bvalero/replacer/finder/Replacement.java
+++ replacer-backend/src/main/java/es/bvalero/replacer/finder/Replacement.java
@@ -1,21 +1,34 @@
 package es.bvalero.replacer.finder;
 
+import java.util.List;
 import lombok.Builder;
 import lombok.Value;
 
-import java.util.List;
-
 /**
- * Domain class of a potential replacement in an article.
+ * A <strong>replacement</strong> is a potential issue to be checked and fixed (replaced). For instance,
+ * the word "aproximated" is misspelled and therefore could be proposed to be replaced with "approximated".
+ *
+ * Note the importance of the <em>potential</em> adjective, as an issue could be just a false positive.
+ * For instance, in Spanish the word "Paris" could be misspelled if it corresponds to the French city
+ * (written correctly as "París"), but it would be correct if it refers to the mythological Trojan prince.
  */
 @Value(staticConstructor = "of")
 @Builder
 public class Replacement implements Comparable<Replacement> {
-
+    /**
+     * The category of the replacement: misspelling, date format, etc.
+     * It may include a subtype, for instance the particular misspelling.
+     */
     private String type;
     private String subtype;
+
+    /** A number corresponding to the position in the page contents where the text is found */
     private int start;
+
+    //** The text to be checked and fixed. It can be a word or an expression. */
     private String text;
+
+    /** At least one suggestion to replace the text */
     private List<Suggestion> suggestions;
 
     public int getEnd() {
@@ -27,5 +40,4 @@ public class Replacement implements Comparable<Replacement> {
         // Order descendant by start. If equals, the lower end.
         return o.start == start ? getEnd() - o.getEnd() : o.start - start;
     }
-
 }
--- replacer-backend/src/main/java/es/bvalero/replacer/finder/Suggestion.java
+++ replacer-backend/src/main/java/es/bvalero/replacer/finder/Suggestion.java
@@ -2,13 +2,18 @@ package es.bvalero.replacer.finder;
 
 import lombok.Value;
 
+/**
+ * A suggestion for a replacement.
+ */
 @Value(staticConstructor = "of")
 public class Suggestion {
+    /** The new text after the replacement */
     private String text;
+
+    /** An optional description to explain the motivation of the fix */
     private String comment;
 
     public static Suggestion ofNoComment(String text) {
         return of(text, null);
     }
-
 }
