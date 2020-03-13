bbdb36f3d64b5b074e570443a08a39cad51e8b9a
Benjamín Valero <benjavalero@gmail.com>
2020-03-13 18:47:38 +0100

    Fix error when authenticating

--- replacer-backend/src/main/java/es/bvalero/replacer/authentication/VerificationToken.java
+++ replacer-backend/src/main/java/es/bvalero/replacer/authentication/VerificationToken.java
@@ -2,8 +2,10 @@ package es.bvalero.replacer.authentication;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 class VerificationToken {
     private RequestToken requestToken;
