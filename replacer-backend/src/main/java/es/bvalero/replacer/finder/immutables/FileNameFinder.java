2c2fd29b2d53c728881b1eb2953b62e4da97588d
Benjamín Valero <benjavalero@gmail.com>
2020-04-04 12:13:28 +0200

    FileNameFinder
    Optimization (linear)

--- replacer-backend/src/main/java/es/bvalero/replacer/finder/immutables/FileNameFinder.java
+++ replacer-backend/src/main/java/es/bvalero/replacer/finder/immutables/FileNameFinder.java
@@ -1,15 +1,10 @@
 package es.bvalero.replacer.finder.immutables;
 
-import dk.brics.automaton.RegExp;
-import dk.brics.automaton.RunAutomaton;
-import es.bvalero.replacer.finder.Immutable;
-import es.bvalero.replacer.finder.ImmutableFinder;
-import es.bvalero.replacer.finder.ImmutableFinderPriority;
-import es.bvalero.replacer.finder.RegexIterable;
+import es.bvalero.replacer.finder.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.MatchResult;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 /**
@@ -23,12 +18,6 @@ public class FileNameFinder implements ImmutableFinder {
     // - Parameter values without File prefix ==> To be managed in ParameterValueFinder
 
     private static final List<String> ALLOWED_PREFIXES = Arrays.asList("Archivo", "File", "Imagen", "Image");
-    private static final String REGEX_FILE_TAG = String.format(
-        "\\[\\[(%s):[^]|]+",
-        StringUtils.join(ALLOWED_PREFIXES, '|')
-    );
-
-    private static final RunAutomaton AUTOMATON_FILE_TAG = new RunAutomaton(new RegExp(REGEX_FILE_TAG).toAutomaton());
 
     @Override
     public ImmutableFinderPriority getPriority() {
@@ -42,13 +31,65 @@ public class FileNameFinder implements ImmutableFinder {
 
     @Override
     public Iterable<Immutable> find(String text) {
-        return new RegexIterable<>(text, AUTOMATON_FILE_TAG, this::convert);
+        return new LinearIterable<>(text, this::findResult, this::convert);
     }
 
-    @Override
-    public Immutable convert(MatchResult match) {
-        String text = match.group();
-        int filenamePos = text.indexOf(':') + 1;
-        return Immutable.of(match.start() + filenamePos, text.substring(filenamePos), this);
+    public MatchResult findResult(String text, int start) {
+        List<MatchResult> matches = new ArrayList<>(100);
+        while (start >= 0 && matches.isEmpty()) {
+            start = findFileName(text, start, matches);
+        }
+        return matches.isEmpty() ? null : matches.get(0);
+    }
+
+    private int findFileName(String text, int start, List<MatchResult> matches) {
+        int startFile = findStartFile(text, start);
+        if (startFile >= 0) {
+            int startFileName = findStartFileName(text, startFile + 2);
+            if (startFileName >= 0) {
+                int endFileName = findEndFile(text, startFileName);
+                if (endFileName >= 0) {
+                    matches.add(LinearMatcher.of(startFileName, text.substring(startFileName, endFileName)));
+                    return endFileName;
+                } else {
+                    return startFileName;
+                }
+            } else {
+                return startFile + 2;
+            }
+        } else {
+            return -1;
+        }
+    }
+
+    private int findStartFile(String text, int start) {
+        return text.indexOf("[[", start);
+    }
+
+    private int findStartFileName(String text, int start) {
+        StringBuilder prefixBuilder = new StringBuilder();
+        for (int i = start; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (ch == '|' || ch == ']') {
+                // Not a file but a hyperlink
+                return -1;
+            } else if (ch == ':') {
+                String prefix = prefixBuilder.toString();
+                return ALLOWED_PREFIXES.contains(prefix) && (i + 1 < text.length()) ? i + 1 : -1;
+            } else {
+                prefixBuilder.append(ch);
+            }
+        }
+        return -1;
+    }
+
+    private int findEndFile(String text, int start) {
+        for (int i = start; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (ch == '|' || ch == ']') {
+                return i;
+            }
+        }
+        return -1;
     }
 }
