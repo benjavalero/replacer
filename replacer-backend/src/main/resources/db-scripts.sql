6e1de15f0227a38985dee5ef3fcb0f3027898b97
Benjamín Valero <benjavalero@gmail.com>
2020-05-04 19:54:57 +0200

    Update DB scripts

--- replacer-backend/src/main/resources/db-scripts.sql
+++ replacer-backend/src/main/resources/db-scripts.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS replacement2;
 CREATE TABLE replacement2 (
     id INT NOT NULL AUTO_INCREMENT,
     article_id INT NOT NULL,
+    lang VARCHAR(2),
     type VARCHAR(25) NOT NULL,
     subtype VARCHAR(30) COLLATE utf8mb4_bin NOT NULL,
     position INT NOT NULL DEFAULT 0,
@@ -13,4 +14,4 @@ CREATE TABLE replacement2 (
     INDEX (article_id)
 );
 
-CREATE INDEX idx_count ON replacement2 (type, subtype, reviewer);
+CREATE INDEX idx_count ON replacement2 (lang, type, subtype, reviewer);
