c0b47b3f9e30097e337c969b30d6eca099cf60e6
Benjamín Valero <benjavalero@gmail.com>
2020-05-17 07:43:30 +0200

    Update local DB connection to new ToolForge domain

--- local-db-tunnel.sh
+++ local-db-tunnel.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh -o "ServerAliveInterval 60" -i ~/.ssh/id_rsa -L 3306:tools-db:3306 -N benjavalero@login.tools.wmflabs.org
+ssh -o "ServerAliveInterval 60" -i ~/.ssh/id_rsa -L 3306:tools-db:3306 -N benjavalero@login.toolforge.org
