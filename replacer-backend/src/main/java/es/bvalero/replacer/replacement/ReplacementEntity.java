dd8f5b17c625b426952255220f13d8dc8411821c
Benjamín Valero <benjavalero@gmail.com>
2020-10-26 17:07:58 +0100

    Remove JPA references

--- replacer-backend/src/main/java/es/bvalero/replacer/replacement/ReplacementEntity.java
+++ replacer-backend/src/main/java/es/bvalero/replacer/replacement/ReplacementEntity.java
@@ -3,52 +3,27 @@ package es.bvalero.replacer.replacement;
 import es.bvalero.replacer.wikipedia.WikipediaLanguage;
 import java.io.Serializable;
 import java.time.LocalDate;
-import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
 import org.jetbrains.annotations.TestOnly;
 
 /**
  * A replacement in the database related to a page.
  */
 @Data
-@NoArgsConstructor // Needed by JPA
+@NoArgsConstructor // Needed by ModelMapper
 @AllArgsConstructor
-@Entity
-@Table(name = "replacement2")
 public class ReplacementEntity implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "article_id")
     private int pageId;
-
-    @Column(length = 2)
     private String lang;
-
-    @Column(nullable = false, length = 25)
     private String type;
-
-    @Column(nullable = false, length = 30)
     private String subtype;
-
-    @Column
-    @ColumnDefault(value = "0")
     private int position;
-
-    @Column
     private String context;
-
-    @Column(nullable = false)
     private LocalDate lastUpdate;
-
-    @Column
     private String reviewer;
-
-    @Column
     private String title;
 
     public ReplacementEntity(int pageId, WikipediaLanguage lang, String type, String subtype, int position) {
--- replacer-backend/src/main/java/module-info.java
+++ replacer-backend/src/main/java/module-info.java
@@ -1,7 +1,7 @@
-open module bvalero.replacer {
+open module es.bvalero.replacer {
+    requires java.activation;
     requires java.annotation;
     requires java.desktop;
-    requires java.persistence;
     requires java.sql;
     requires java.xml.bind;
 
@@ -13,7 +13,6 @@ open module bvalero.replacer {
     requires spring.context;
     requires spring.core;
     requires spring.data.commons;
-    requires spring.data.jpa;
     requires spring.jdbc;
     requires spring.oxm;
     requires spring.tx;
@@ -23,7 +22,6 @@ open module bvalero.replacer {
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jdk8;
-    requires org.hibernate.orm.core;
     requires org.slf4j;
 
     requires automaton;
