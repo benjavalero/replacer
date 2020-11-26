2652d9d0c4b80df553196d23a49c2682ddb25c7f
Benjamín Valero <benjavalero@gmail.com>
2020-11-26 16:45:58 +0100

    Move logback configuration to XML. Add appender to Logz in Production.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cp replacer-backend/target/replacer.jar $HOME/
+# cp replacer-backend/target/replacer.jar $HOME/
 cd $HOME
-exec java -XX:+HeapDumpOnOutOfMemoryError -jar $HOME/replacer.jar
+exec java -Djdk.xml.totalEntitySizeLimit=0 -XX:+HeapDumpOnOutOfMemoryError -DLOGZ_TOKEN=xxxxxx -jar $HOME/replacer.jar
