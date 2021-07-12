d2099121021a7086d9e4ce4cd42d8974f270bcd0
Benjamín Valero <benjavalero@gmail.com>
2021-07-12 20:17:35 +0200

    Upgrade to Angular 12

--- replacer-frontend/src/app/app.component.spec.ts
+++ replacer-frontend/src/app/app.component.spec.ts
@@ -1,18 +1,14 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
