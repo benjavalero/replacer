1bf317ffeec850e1386b69e5caedb0f0a888cbe0
Benjamín Valero <benjavalero@gmail.com>
2021-08-08 16:11:23 +0200

    Test Wikipedia date truncation

--- replacer-backend/src/test/java/es/bvalero/replacer/wikipedia/WikipediaDateUtilsTest.java
+++ replacer-backend/src/test/java/es/bvalero/replacer/wikipedia/WikipediaDateUtilsTest.java
@@ -2,6 +2,7 @@ package es.bvalero.replacer.wikipedia;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.temporal.ChronoUnit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +21,16 @@ class WikipediaDateUtilsTest {
             expected,
             WikipediaDateUtils.formatWikipediaTimestamp(LocalDateTime.of(2018, Month.AUGUST, 31, 5, 17, 28))
         );
+    }
+
+    @Test
+    void testReversibility() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime truncated = now.truncatedTo(ChronoUnit.SECONDS);
+        LocalDateTime reversed = WikipediaDateUtils.parseWikipediaTimestamp(
+            WikipediaDateUtils.formatWikipediaTimestamp(now)
+        );
+        Assertions.assertNotEquals(now, reversed);
+        Assertions.assertEquals(truncated, reversed);
     }
 }
