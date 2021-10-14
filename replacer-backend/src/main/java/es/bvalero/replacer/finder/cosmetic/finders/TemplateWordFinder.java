4edaf19ca99b0c25703ccd0688f1cbebfa68c654
Benjamín Valero <benjavalero@gmail.com>
2021-10-14 12:02:43 +0200

    Import static in template-word finder

--- replacer-backend/src/main/java/es/bvalero/replacer/finder/cosmetic/finders/TemplateWordFinder.java
+++ replacer-backend/src/main/java/es/bvalero/replacer/finder/cosmetic/finders/TemplateWordFinder.java
@@ -1,5 +1,8 @@
 package es.bvalero.replacer.finder.cosmetic.finders;
 
+import static es.bvalero.replacer.finder.util.TemplateUtils.END_TEMPLATE;
+import static es.bvalero.replacer.finder.util.TemplateUtils.START_TEMPLATE;
+
 import es.bvalero.replacer.finder.FinderPage;
 import es.bvalero.replacer.finder.cosmetic.CosmeticCheckedFinder;
 import es.bvalero.replacer.finder.cosmetic.checkwikipedia.CheckWikipediaAction;
@@ -27,11 +30,11 @@ class TemplateWordFinder extends CosmeticCheckedFinder {
     @Override
     public boolean validate(MatchResult match, FinderPage page) {
         String templateText = match.group();
-        assert templateText.startsWith(TemplateUtils.START_TEMPLATE);
-        int posColon = templateText.indexOf(':', TemplateUtils.START_TEMPLATE.length());
-        if (posColon >= TemplateUtils.START_TEMPLATE.length()) {
+        assert templateText.startsWith(START_TEMPLATE);
+        int posColon = templateText.indexOf(':', START_TEMPLATE.length());
+        if (posColon >= START_TEMPLATE.length()) {
             String langTemplateWord = templateWords.get(page.getLang().getCode());
-            String templateWord = templateText.substring(2, posColon).trim();
+            String templateWord = templateText.substring(START_TEMPLATE.length(), posColon).trim();
             return langTemplateWord.equalsIgnoreCase(templateWord);
         }
         return false;
@@ -45,20 +48,20 @@ class TemplateWordFinder extends CosmeticCheckedFinder {
     @Override
     public String getFix(MatchResult match) {
         String templateText = match.group();
-        int posColon = templateText.indexOf(':', TemplateUtils.START_TEMPLATE.length());
-        assert posColon >= TemplateUtils.START_TEMPLATE.length();
-        assert templateText.endsWith(TemplateUtils.END_TEMPLATE);
+        int posColon = templateText.indexOf(':', START_TEMPLATE.length());
+        assert posColon >= START_TEMPLATE.length();
+        assert templateText.endsWith(END_TEMPLATE);
         String afterColon = templateText.substring(posColon + 1).trim();
-        if (afterColon.startsWith(TemplateUtils.START_TEMPLATE)) {
-            String innerTemplate = afterColon.substring(0, afterColon.length() - TemplateUtils.END_TEMPLATE.length());
-            if (innerTemplate.endsWith(TemplateUtils.END_TEMPLATE)) {
+        if (afterColon.startsWith(START_TEMPLATE)) {
+            String innerTemplate = afterColon.substring(0, afterColon.length() - END_TEMPLATE.length());
+            if (innerTemplate.endsWith(END_TEMPLATE)) {
                 return innerTemplate;
             } else {
                 LOGGER.error("Unsupported template word case: {}", templateText);
                 return templateText;
             }
         } else {
-            return TemplateUtils.START_TEMPLATE + afterColon;
+            return START_TEMPLATE + afterColon;
         }
     }
 }
