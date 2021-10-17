67160889b618e544758ca50a773cb0beaa58e47e
Benjamín Valero <benjavalero@gmail.com>
2021-10-17 13:35:58 +0200

    Simplify category-in-English finder

--- replacer-backend/src/main/java/es/bvalero/replacer/finder/cosmetic/finders/CategoryEnglishFinder.java
+++ replacer-backend/src/main/java/es/bvalero/replacer/finder/cosmetic/finders/CategoryEnglishFinder.java
@@ -1,41 +1,34 @@
 package es.bvalero.replacer.finder.cosmetic.finders;
 
-import static es.bvalero.replacer.finder.util.LinkUtils.START_LINK;
-
 import es.bvalero.replacer.finder.FinderPage;
 import es.bvalero.replacer.finder.cosmetic.CosmeticCheckedFinder;
 import es.bvalero.replacer.finder.cosmetic.checkwikipedia.CheckWikipediaAction;
-import es.bvalero.replacer.finder.util.LinkUtils;
-import java.util.ArrayList;
+import es.bvalero.replacer.finder.util.RegexMatchFinder;
 import java.util.Map;
 import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
 import javax.annotation.Resource;
+import org.intellij.lang.annotations.RegExp;
 import org.springframework.stereotype.Component;
 
 /** Find categories in English */
 @Component
 class CategoryEnglishFinder extends CosmeticCheckedFinder {
 
-    private static final String CATEGORY_ENGLISH_WORD = "category";
+    @RegExp
+    private static final String REGEX_CATEGORY_ENGLISH = "\\[\\[\\s*Category\\s*:(.+?)]]";
+
+    private static final Pattern PATTERN_CATEGORY_ENGLISH = Pattern.compile(
+        REGEX_CATEGORY_ENGLISH,
+        Pattern.CASE_INSENSITIVE
+    );
 
     @Resource
     private Map<String, String> categoryWords;
 
     @Override
     public Iterable<MatchResult> findMatchResults(FinderPage page) {
-        return new ArrayList<>(LinkUtils.findAllLinks(page));
-    }
-
-    @Override
-    public boolean validate(MatchResult match, FinderPage page) {
-        String templateText = match.group();
-        assert templateText.startsWith(START_LINK);
-        int posColon = templateText.indexOf(':', START_LINK.length());
-        if (posColon >= START_LINK.length()) {
-            String templateName = templateText.substring(START_LINK.length(), posColon);
-            return CATEGORY_ENGLISH_WORD.equalsIgnoreCase(templateName);
-        }
-        return false;
+        return RegexMatchFinder.find(page.getContent(), PATTERN_CATEGORY_ENGLISH);
     }
 
     @Override
@@ -45,9 +38,8 @@ class CategoryEnglishFinder extends CosmeticCheckedFinder {
 
     @Override
     public String getFix(MatchResult match, FinderPage page) {
-        String templateText = match.group();
-        int posColon = templateText.indexOf(':', START_LINK.length());
         String categoryWord = categoryWords.get(page.getLang().getCode());
-        return START_LINK + categoryWord + templateText.substring(posColon);
+        String templateContent = match.group(1).replace("_", " ").trim();
+        return String.format("[[%s:%s]]", categoryWord, templateContent);
     }
 }
