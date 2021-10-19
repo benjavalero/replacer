906a7522ed741175d788b773c64b44ff6ba1079b
Benjamín Valero <benjavalero@gmail.com>
2021-10-19 12:39:18 +0200

    Merge category-in-English finder with space-not-translated finder

--- replacer-backend/src/main/java/es/bvalero/replacer/finder/cosmetic/finders/SpaceNotTranslatedFinder.java
+++ replacer-backend/src/main/java/es/bvalero/replacer/finder/cosmetic/finders/SpaceNotTranslatedFinder.java
@@ -1,7 +1,8 @@
 package es.bvalero.replacer.finder.cosmetic.finders;
 
 import es.bvalero.replacer.finder.FinderPage;
-import es.bvalero.replacer.finder.cosmetic.CosmeticFinder;
+import es.bvalero.replacer.finder.cosmetic.CosmeticCheckedFinder;
+import es.bvalero.replacer.finder.cosmetic.checkwikipedia.CheckWikipediaAction;
 import es.bvalero.replacer.finder.util.FinderUtils;
 import es.bvalero.replacer.finder.util.RegexMatchFinder;
 import java.util.Map;
@@ -15,11 +16,12 @@ import org.springframework.stereotype.Component;
 
 /** Find space links where the space is not translated */
 @Component
-class SpaceNotTranslatedFinder implements CosmeticFinder {
+class SpaceNotTranslatedFinder extends CosmeticCheckedFinder {
 
     private static final String FILE_SPACE_EN = "File";
     private static final String IMAGE_SPACE_EN = "Image";
     private static final String ANNEX_SPACE_EN = "Annex";
+    private static final String CATEGORY_SPACE_EN = "Category";
 
     @RegExp
     private static final String REGEX_SPACE = "\\[\\[(%s):(.+?)]]";
@@ -33,11 +35,14 @@ class SpaceNotTranslatedFinder implements CosmeticFinder {
     @Resource
     private Map<String, String> annexWords;
 
+    @Resource
+    private Map<String, String> categoryWords;
+
     private Pattern patternLowercaseSpace;
 
     @PostConstruct
     public void init() {
-        Set<String> spaceWords = Set.of(FILE_SPACE_EN, IMAGE_SPACE_EN, ANNEX_SPACE_EN);
+        Set<String> spaceWords = Set.of(FILE_SPACE_EN, IMAGE_SPACE_EN, ANNEX_SPACE_EN, CATEGORY_SPACE_EN);
         String concat = String.join("|", spaceWords);
         String regex = String.format(REGEX_SPACE, FinderUtils.toLowerCase(concat));
         patternLowercaseSpace = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
@@ -49,16 +54,28 @@ class SpaceNotTranslatedFinder implements CosmeticFinder {
     }
 
     @Override
+    protected CheckWikipediaAction getCheckWikipediaAction() {
+        // We return this action if the space fixed is not a Category
+        return CheckWikipediaAction.CATEGORY_IN_ENGLISH;
+    }
+
+    @Override
     public String getFix(MatchResult match, FinderPage page) {
         String spaceWord = FinderUtils.setFirstUpperCase(match.group(1));
         String spaceWordTranslated = null;
-        if (FILE_SPACE_EN.equals(spaceWord)) {
-            spaceWordTranslated = fileWords.get(page.getLang().getCode());
-        } else if (IMAGE_SPACE_EN.equals(spaceWord)) {
-            spaceWordTranslated = imageWords.get(page.getLang().getCode());
-        }
-        if (ANNEX_SPACE_EN.equals(spaceWord)) {
-            spaceWordTranslated = annexWords.get(page.getLang().getCode());
+        switch (spaceWord) {
+            case FILE_SPACE_EN:
+                spaceWordTranslated = fileWords.get(page.getLang().getCode());
+                break;
+            case IMAGE_SPACE_EN:
+                spaceWordTranslated = imageWords.get(page.getLang().getCode());
+                break;
+            case ANNEX_SPACE_EN:
+                spaceWordTranslated = annexWords.get(page.getLang().getCode());
+                break;
+            case CATEGORY_SPACE_EN:
+                spaceWordTranslated = categoryWords.get(page.getLang().getCode());
+                break;
         }
         assert spaceWordTranslated != null;
 
--- replacer-backend/src/test/java/es/bvalero/replacer/finder/cosmetic/finders/SpaceNotTranslatedFinderTest.java
+++ replacer-backend/src/test/java/es/bvalero/replacer/finder/cosmetic/finders/SpaceNotTranslatedFinderTest.java
@@ -6,21 +6,36 @@ import es.bvalero.replacer.common.WikipediaLanguage;
 import es.bvalero.replacer.config.XmlConfiguration;
 import es.bvalero.replacer.finder.FinderPage;
 import es.bvalero.replacer.finder.cosmetic.Cosmetic;
+import es.bvalero.replacer.finder.cosmetic.checkwikipedia.CheckWikipediaOfflineService;
+import es.bvalero.replacer.finder.cosmetic.checkwikipedia.CheckWikipediaService;
 import java.util.List;
 import org.apache.commons.collections4.IterableUtils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = { SpaceNotTranslatedFinder.class, XmlConfiguration.class })
+@ActiveProfiles("offline")
+@SpringBootTest(
+    classes = { SpaceNotTranslatedFinder.class, XmlConfiguration.class, CheckWikipediaOfflineService.class }
+)
 class SpaceNotTranslatedFinderTest {
+
+    @Autowired
+    private CheckWikipediaService checkWikipediaService;
 
     @Autowired
     private SpaceNotTranslatedFinder spaceNotTranslatedFinder;
 
     @ParameterizedTest
-    @CsvSource(value = { "[[File:x.jpeg|test]], [[Archivo:x.jpeg|test]]", "[[image:x.png]], [[Imagen:x.png]]" })
+    @CsvSource(
+        value = {
+            "[[File:x.jpeg|test]], [[Archivo:x.jpeg|test]]",
+            "[[image:x.png]], [[Imagen:x.png]]",
+            "[[Category:Animal]], [[Categoría:Animal]]",
+        }
+    )
     void testNotTranslatedSpaceFinder(String text, String fix) {
         List<Cosmetic> cosmetics = spaceNotTranslatedFinder.findList(text);
 
