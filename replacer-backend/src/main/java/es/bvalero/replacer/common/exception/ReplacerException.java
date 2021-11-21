d502c21c7197ff60ef9c5f1ffc2230495db28107
Benjamín Valero <benjavalero@gmail.com>
2021-11-21 09:32:38 +0100

    Refactor: common exceptions

--- replacer-backend/src/main/java/es/bvalero/replacer/common/exception/ReplacerException.java
+++ replacer-backend/src/main/java/es/bvalero/replacer/common/exception/ReplacerException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
- * Exception to be used in the context of Replacer.
- * It is not worth to define sub-exceptions by functionality.
- * In case of throwing in Controllers is identified as an error 500.
+ * The exception to be used in the context of all the application.
+ * In general, it is not worth to define sub-exceptions by functionality, but it can be done in some sub-domains.
+ * In case of being thrown by Controllers, it is identified as an HTTP error 500.
  */
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class ReplacerException extends Exception {
--- replacer-backend/src/main/java/es/bvalero/replacer/common/exception/UnauthorizedException.java
+++ replacer-backend/src/main/java/es/bvalero/replacer/common/exception/UnauthorizedException.java
@@ -3,11 +3,6 @@ package es.bvalero.replacer.common.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/** Exception for operations not allowed according to the user roles or rights */
+/** Exception for operations in Controllers not allowed according to the user roles or rights */
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class UnauthorizedException extends Exception {
-
-    public UnauthorizedException() {
-        super();
-    }
-}
+public class UnauthorizedException extends ReplacerException {}
