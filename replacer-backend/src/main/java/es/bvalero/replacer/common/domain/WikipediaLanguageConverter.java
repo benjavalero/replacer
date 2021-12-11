45d4608249c8a717c1810c5d7b138e5c836638c0
Benjamín Valero <benjavalero@gmail.com>
2021-12-11 11:41:24 +0100

    Upgrade Spring Boot version to 2.5

--- replacer-backend/src/main/java/es/bvalero/replacer/common/domain/WikipediaLanguageConverter.java
+++ replacer-backend/src/main/java/es/bvalero/replacer/common/domain/WikipediaLanguageConverter.java
@@ -3,8 +3,9 @@ package es.bvalero.replacer.common.domain;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-// This converter is needed because of a bug on Jackson library.
-// See https://github.com/FasterXML/jackson-databind/issues/1850
+// This converter is needed because Spring doesn't use Jackson for web-bindings,
+// only for the body request and the response.
+// See https://github.com/spring-projects/spring-boot/issues/24233#issuecomment-733001230
 @Component
 public class WikipediaLanguageConverter implements Converter<String, WikipediaLanguage> {
 
