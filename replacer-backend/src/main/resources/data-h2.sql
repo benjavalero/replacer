cb2233e0aa8729ee134c27d8a35e2ff1d8027302
Benjamín Valero <benjavalero@gmail.com>
2021-12-21 10:48:11 +0100

    Adapt H2 offline scripts to recent changes in database schema

--- replacer-backend/src/main/resources/data-h2.sql
+++ replacer-backend/src/main/resources/data-h2.sql
@@ -1,17 +1,17 @@
 INSERT INTO page (article_id, lang, title, last_update)
   VALUES (1, 'es', 'Norteamérica', NOW());
 
-INSERT INTO replacement (article_id, lang, type, subtype, context, last_update)
-  VALUES (1, 'es', 'Ortografía', 'triada', '', NOW());
-INSERT INTO replacement (article_id, lang, type, subtype, context, last_update)
-  VALUES (1, 'es', 'Ortografía', 'sólo', '', NOW());
-INSERT INTO replacement (article_id, lang, type, subtype, context, last_update)
-  VALUES (1, 'es', 'Compuestos', 'aún así', '', NOW());
-INSERT INTO replacement (article_id, lang, type, subtype, context, last_update)
-  VALUES (1, 'es', 'Fechas', 'Mes en mayúscula', '', NOW());
-INSERT INTO replacement (article_id, lang, type, subtype, context, last_update)
-  VALUES (1, 'es', 'Ortografía', 'publica', '', NOW());
-INSERT INTO replacement (article_id, lang, type, subtype, context, last_update)
-  VALUES (1, 'es', 'Ortografía', 'status', '', NOW());
-INSERT INTO replacement (article_id, lang, type, subtype, context, last_update)
-  VALUES (1, 'es', 'Ortografía', 'reference', '', NOW());
+INSERT INTO replacement (article_id, lang, type, subtype, context)
+  VALUES (1, 'es', 'Ortografía', 'triada', '');
+INSERT INTO replacement (article_id, lang, type, subtype, context)
+  VALUES (1, 'es', 'Ortografía', 'sólo', '');
+INSERT INTO replacement (article_id, lang, type, subtype, context)
+  VALUES (1, 'es', 'Compuestos', 'aún así', '');
+INSERT INTO replacement (article_id, lang, type, subtype, context)
+  VALUES (1, 'es', 'Fechas', 'Mes en mayúscula', '');
+INSERT INTO replacement (article_id, lang, type, subtype, context)
+  VALUES (1, 'es', 'Ortografía', 'publica', '');
+INSERT INTO replacement (article_id, lang, type, subtype, context)
+  VALUES (1, 'es', 'Ortografía', 'status', '');
+INSERT INTO replacement (article_id, lang, type, subtype, context)
+  VALUES (1, 'es', 'Ortografía', 'reference', '');
