75e99a2af7878ba2a8f2925b5208230729352136
Benjamín Valero <benjavalero@gmail.com>
2023-04-30 17:55:50 +0200

    Make Angular root module standalone too

--- replacer-frontend/src/app/app.component.ts
+++ replacer-frontend/src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { AlertContainerComponent } from './shared/alert/alert-container.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
+  imports: [HeaderComponent, AlertContainerComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- replacer-frontend/src/main.ts
+++ replacer-frontend/src/main.ts
@@ -1,9 +1,28 @@
 /// <reference types="@angular/localize" />
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import { ApiModule } from './app/api/api.module';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { LangInterceptor } from './app/core/interceptor/lang-interceptor';
+import { UserInterceptor } from './app/core/interceptor/user-interceptor';
+import { environment } from './environments/environment';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule, ApiModule.forRoot({ rootUrl: environment.baseUrl })),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LangInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: UserInterceptor,
+      multi: true
+    },
+    provideHttpClient(withInterceptorsFromDi())
+  ]
+}).catch((err) => console.error(err));
