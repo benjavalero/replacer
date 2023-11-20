e266fe3f1bb182d36e787362148457e4c9a6d47c
Benjamín Valero <benjavalero@gmail.com>
2023-11-20 22:57:17 +0100

    Add frontend configuration directly on main.ts

--- replacer-frontend/src/main.ts
+++ replacer-frontend/src/main.ts
@@ -1,8 +1,30 @@
 /// <reference types="@angular/localize" />
 
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { ApiModule } from './app/api/api.module';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { ErrorInterceptor } from './app/core/interceptor/error-interceptor';
+import { LangInterceptor } from './app/core/interceptor/lang-interceptor';
+import { environment } from './environments/environment';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(ApiModule.forRoot({ rootUrl: environment.baseUrl })),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LangInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
+  ]
+}).catch((err) => console.error(err));
