0b94ade0ffcb4379ed4d20e903fd47beecf4c553
Benjamín Valero <benjavalero@gmail.com>
2025-05-02 14:11:11 +0200

    Config: add Spring Modulith dependencies and tests
    For the moment we ignore the architecture tests when building the project

--- replacer-backend/replacer-app/src/test/java/es/bvalero/replacer/ArchitectureTest.java
+++ replacer-backend/replacer-app/src/test/java/es/bvalero/replacer/ArchitectureTest.java
@@ -4,6 +4,9 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import org.jmolecules.archunit.JMoleculesArchitectureRules;
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @AnalyzeClasses(packages = "es.bvalero.replacer")
 class ArchitectureTest {
@@ -14,4 +17,22 @@ class ArchitectureTest {
 
     @ArchTest
     ArchRule hexagonal = JMoleculesArchitectureRules.ensureHexagonal();
+
+    @Test
+    void createApplicationModuleModel() {
+        ApplicationModules modules = ApplicationModules.of(Replacer.class);
+        modules.forEach(System.out::println);
+    }
+
+    @Test
+    void verifiesModuleStructure() {
+        ApplicationModules modules = ApplicationModules.of(Replacer.class);
+        modules.verify();
+    }
+
+    @Test
+    void createModuleDocumentation() {
+        ApplicationModules modules = ApplicationModules.of(Replacer.class);
+        new Documenter(modules).writeModulesAsPlantUml().writeIndividualModulesAsPlantUml();
+    }
 }
