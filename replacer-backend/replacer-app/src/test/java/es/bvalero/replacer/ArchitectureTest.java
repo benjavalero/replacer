1789435c3a3b565fdd7247c8d55a412da76f4e6b
Benjamín Valero <benjavalero@gmail.com>
2025-06-10 10:23:14 +0200

    Config: add Spring Modulith dependencies and tests

--- replacer-backend/replacer-app/src/test/java/es/bvalero/replacer/ArchitectureTest.java
+++ replacer-backend/replacer-app/src/test/java/es/bvalero/replacer/ArchitectureTest.java
@@ -4,14 +4,24 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import org.jmolecules.archunit.JMoleculesArchitectureRules;
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
 
 @AnalyzeClasses(packages = "es.bvalero.replacer")
 class ArchitectureTest {
 
-    // TODO: All core use cases must implement an interface which will belong to the domain module
-    // TODO: Adapter modules (in particular, Web) will only depend on domain module
-    // TODO: We don't need architecture tests anymore
-
     @ArchTest
     ArchRule hexagonal = JMoleculesArchitectureRules.ensureHexagonal();
+
+    @Test
+    void createApplicationModuleModel() {
+        ApplicationModules modules = ApplicationModules.of(Replacer.class);
+        modules.forEach(System.out::println);
+    }
+
+    @Test
+    void verifiesModuleStructure() {
+        ApplicationModules modules = ApplicationModules.of(Replacer.class);
+        modules.verify();
+    }
 }
