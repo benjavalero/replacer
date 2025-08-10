764fdcfe7840fcd505957f48e05171ed0dc3adaf
Benjamín Valero <benjavalero@gmail.com>
2025-08-10 09:38:56 +0200

    Refactor: architecture test to create module documentation

--- replacer-backend/replacer-app/src/test/java/es/bvalero/replacer/ArchitectureTest.java
+++ replacer-backend/replacer-app/src/test/java/es/bvalero/replacer/ArchitectureTest.java
@@ -6,6 +6,7 @@ import com.tngtech.archunit.lang.ArchRule;
 import org.jmolecules.archunit.JMoleculesArchitectureRules;
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @AnalyzeClasses(packages = "es.bvalero.replacer")
 class ArchitectureTest {
@@ -23,5 +24,11 @@ class ArchitectureTest {
     void verifiesModuleStructure() {
         ApplicationModules modules = ApplicationModules.of(Replacer.class);
         modules.verify();
+    }
+
+    @Test
+    void createModuleDocumentation() {
+        ApplicationModules modules = ApplicationModules.of(Replacer.class);
+        new Documenter(modules).writeModulesAsPlantUml().writeIndividualModulesAsPlantUml();
     }
 }
