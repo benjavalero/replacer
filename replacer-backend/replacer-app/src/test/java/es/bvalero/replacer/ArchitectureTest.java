ac7c645a0682dd151840c40e5592817761c76df2
Benjamín Valero <benjavalero@gmail.com>
2025-08-13 11:13:22 +0200

    Refactor: exclude common module on module verification and documentation

--- replacer-backend/replacer-app/src/test/java/es/bvalero/replacer/ArchitectureTest.java
+++ replacer-backend/replacer-app/src/test/java/es/bvalero/replacer/ArchitectureTest.java
@@ -1,5 +1,6 @@
 package es.bvalero.replacer;
 
+import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
@@ -16,19 +17,26 @@ class ArchitectureTest {
 
     @Test
     void createApplicationModuleModel() {
-        ApplicationModules modules = ApplicationModules.of(Replacer.class);
+        ApplicationModules modules = buildApplicationModules();
         modules.forEach(System.out::println);
     }
 
     @Test
     void verifiesModuleStructure() {
-        ApplicationModules modules = ApplicationModules.of(Replacer.class);
+        ApplicationModules modules = buildApplicationModules();
         modules.verify();
     }
 
     @Test
     void createModuleDocumentation() {
-        ApplicationModules modules = ApplicationModules.of(Replacer.class);
+        ApplicationModules modules = buildApplicationModules();
         new Documenter(modules).writeModulesAsPlantUml().writeIndividualModulesAsPlantUml();
+    }
+
+    private ApplicationModules buildApplicationModules() {
+        return ApplicationModules.of(
+            Replacer.class,
+            JavaClass.Predicates.resideInAPackage("es.bvalero.replacer.common..")
+        );
     }
 }
--- replacer-backend/replacer-domain/src/main/java/es/bvalero/replacer/common/package-info.java
+++
@@ -1,5 +0,0 @@
-// TODO: We could remove this annotation by placing all the classes in the same folder
-@org.springframework.modulith.ApplicationModule(type = ApplicationModule.Type.OPEN)
-package es.bvalero.replacer.common;
-
-import org.springframework.modulith.ApplicationModule;
